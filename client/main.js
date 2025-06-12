1f173ab087aafd601c09f931edcd33ea5888980e
CaPiTo26 <CaPiTo26@gmail.com>
2025-06-12 22:28:55 +0200

    Initial Commit

--- client/main.js
+++ client/main.js
@@ -1,9 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import App from '../imports/ui/App.svelte';
-
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '/imports/ui/App';
 
 Meteor.startup(() => {
-  new App({
-    target: document.getElementById('app')
-  });
+  const container = document.getElementById('react-target');
+  const root = createRoot(container);
+  root.render(<App />);
 });
